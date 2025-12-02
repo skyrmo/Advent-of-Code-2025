18a1c25561400909a30e1212ccada76660574d66
Skyrmo <owaingskyrme@gmail.com>
2025-12-02 15:55:10 +0800

    Completed Day 2

--- day_02/day_02_part_1.py
+++ day_02/day_02_part_1.py
@@ -1,15 +1,15 @@
-
-import os
 import collections
+import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split(",")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +19,34 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    # Implement solution here
-    pass
+    # print(input_data)
+    result = 0
+
+    for rng in input_data:
+        s, e = rng.split("-")
+        start, end = int(s), int(e)
+        # print(rng, start, end)
+        for i in range(start, end + 1):
+            # print(i)
+            i_str = str(i)
+            i_len = len(i_str)
+            if len(i_str) % 2 == 1:
+                continue
+            if i_str[: i_len // 2] == i_str[i_len // 2 :]:
+                print(i, i_str[: i_len // 2], i_str[i_len // 2 :])
+                result += i
+
+    return result
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +55,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 02, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- day_02/day_02_part_2.py
+++ day_02/day_02_part_2.py
@@ -1,15 +1,15 @@
-
-import os
 import collections
+import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split(",")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,23 +19,72 @@ def parse_input(file_path):
 
         return data
 
+
+def get_factors(n):
+    factors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            factors.append(i)
+            if i != n // i:
+                factors.append(n // i)
+    return sorted(factors)[:-1]
+
+
+def check(num: str):
+    n = len(num)
+    factors = get_factors(n)
+
+    # print(num, n, factors)
+
+    is_repeated = False
+
+    for factor in factors[::-1]:
+        is_repeated = True
+        for i in range(factor):
+            for j in range(1, n // factor):
+                tgt_idx = i + j * factor
+                if num[i] != num[tgt_idx]:
+                    is_repeated = False
+                    break
+
+            if not is_repeated:
+                break
+
+        if is_repeated:
+            return True
+
+    return is_repeated
+
+
 def solve(input_data):
-    # Implement solution here
-    pass
+    result = 0
+
+    for rng in input_data:
+        s, e = rng.split("-")
+        start, end = int(s), int(e)
+
+        for i in range(start, end + 1):
+            i_str = str(i)
+            if check(i_str):
+                result += i
+
+    return result
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    input_path = os.path.join(script_dir, 'input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
 
     # Solve and print the solution
     result = solve(parsed_input)
-    print(f"Solution for Day 02, Part Two: {result}")
+    print(f"Solution for Day 02, Part One: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
